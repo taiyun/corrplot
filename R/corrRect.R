738fb2b829ccfde007c43faec560c48d117e4667
Viliam Simko <viliam.simko@gmail.com>
2016-02-26 15:25:50 +0100

    extracted example from corrRect.R to a separate file

--- R/corrRect.R
+++ R/corrRect.R
@@ -18,38 +18,7 @@
 #'   \code{"single"}, \code{"complete"}, \code{"average"}, \code{"mcquitty"},
 #'   \code{"median"} or \code{"centroid"}.
 #'
-#' @examples
-#' data(mtcars)
-#' M <- cor(mtcars)
-#' corrplot(M, method = "circle", order = "FPC")
-#' corrRect(c(5,6))
-#'
-#' (order.hc <- corrMatOrder(M, order = "hclust"))
-#' (order.hc2 <- corrMatOrder(M, order = "hclust", hclust.method = "ward"))
-#' M.hc  <- M[order.hc, order.hc ]
-#' M.hc2 <- M[order.hc2,order.hc2]
-#'
-#' par(ask = TRUE)
-#' ##same as: corrplot(M, order = "hclust", addrect = 2)
-#' corrplot(M.hc)
-#' corrRect.hclust(corr = M.hc, k = 2)
-#'
-#' ##same as: corrplot(M, order = "hclust", addrect = 3)
-#' corrplot(M.hc)
-#' corrRect.hclust(corr = M.hc, k = 3)
-#'
-#' ##same as: corrplot(M, order = "hclust", hclust.method = "ward", addrect = 2)
-#' corrplot(M.hc2)
-#' corrRect.hclust(M.hc2, k = 2, method = "ward")
-#'
-#' ##same as: corrplot(M, order = "hclust", hclust.method = "ward", addrect = 3)
-#' corrplot(M.hc2)
-#' corrRect.hclust(M.hc2, k = 3, method = "ward")
-#'
-#' ##same as: corrplot(M, order = "hclust", hclust.method = "ward", addrect = 4)
-#' corrplot(M.hc2)
-#' corrRect.hclust(M.hc2, k = 4, method = "ward")
-#'
+#' @example vignettes/example-corrRect.R
 #' @keywords hplot
 #' @author Taiyun Wei
 #' @export
