d167c2267c0d1da7234b3f2210f00cb29bcead48
Viliam Simko <viliam.simko@gmail.com>
2016-02-26 15:11:19 +0100

    corrMatOrder.R : roxygen + doccomment

---
+++ vignettes/example-corrplot.mixed.R
@@ -0,0 +1,13 @@
+M <- cor(mtcars)
+ord <- corrMatOrder(M, order = "AOE")
+M2 <- M[ord,ord]
+
+par(ask = TRUE)
+corrplot.mixed(M2)
+corrplot.mixed(M2, lower = "ellipse", upper = "circle")
+corrplot.mixed(M2, lower = "square", upper = "circle")
+corrplot.mixed(M2, lower = "shade", upper = "circle")
+corrplot.mixed(M2, tl.pos = "lt")
+corrplot.mixed(M2, tl.pos = "lt", diag = "u")
+corrplot.mixed(M2, tl.pos = "lt", diag = "l")
+corrplot.mixed(M2, tl.pos = "n")
