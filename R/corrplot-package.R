c8135b51a8801a63b3c67f9e9439a6d3fb6053f1
Viliam Simko <viliam.simko@gmail.com>
2017-02-09 16:38:23 +0100

    Converted corrplot-package description from Rd to R and using roxygen to generate the Rd file automagically.

---
+++ R/corrplot-package.R
@@ -0,0 +1,40 @@
+#' @docType package
+#' @name corrplot-package
+#' @aliases corrplot
+#'
+#' @title
+#' Visualization of a correlation matrix
+#'
+#' @description
+#' The corrplot package is a graphical display of a correlation matrix,
+#' confidence interval. It also contains some algorithms to do matrix
+#' reordering.
+#'
+#' @author Taiyun Wei (weitaiyun@@gmail.com)
+#' @author Viliam Simko (viliam.simko@@gmail.com)
+#'
+#' Maintainer: Taiyun Wei (weitaiyun@@gmail.com)
+#'
+#' @references
+#' Michael Friendly (2002).
+#' \emph{Corrgrams: Exploratory displays for correlation matrices}.
+#' The American Statistician, 56, 316--324.
+#'
+#' D.J. Murdoch, E.D. Chow (1996).
+#' \emph{A graphical display of large correlation matrices}.
+#' The American Statistician, 50, 178--180.
+#'
+#' @seealso
+#' The \code{plotcorr} function in the \code{ellipse} package and
+#' \code{corrgram} function in the \code{corrgram} package has some
+#' similarities.
+#'
+#' @keywords hplot
+#' @keywords correlation
+NULL
+
+.onAttach <- function(libname, pkgname) {
+  # just to show a startup message
+  message <- paste("corrplot", utils::packageVersion("corrplot"), "loaded")
+  packageStartupMessage(message, appendLF = TRUE)
+}
