5906f2a176a55d0a13ac7bacfff62c631923fbf7
Viliam Simko <viliam.simko@gmail.com>
2017-08-29 15:17:06 +0200

    added some tests for `cor.mtest()` function

---
+++ tests/testthat/test-cor-mtest.R
@@ -0,0 +1,25 @@
+context("cor.mtest - significance test and confidence intervals")
+
+test_that("Basic usage of cor.mtest", {
+
+  res1 <- cor.mtest(mtcars)
+
+  expect_true(is.list(res1))
+  expect_true(is.matrix(res1$p))
+  expect_true(is.matrix(res1$lowCI))
+  expect_true(is.matrix(res1$uppCI))
+
+  expect_equal(dim(res1$p), c(11,11))
+
+  expect_equal(dim(res1$p), dim(res1$lowCI))
+  expect_equal(dim(res1$p), dim(res1$uppCI))
+
+})
+
+test_that("Additional params", {
+  expect_silent(cor.mtest(mtcars, conf.level = 0.95))
+  expect_silent(cor.mtest(mtcars, method = "spearman", exact = FALSE))
+
+  # unknown parameters are silently ignored
+  expect_silent(cor.mtest(mtcars, dummy = "dummy"))
+})
