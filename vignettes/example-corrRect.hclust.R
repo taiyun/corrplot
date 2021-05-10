c105b74fc249f16aa4774446fef0df0822ba1fa3
taiyun <weitaiyun@qq.com>
2021-05-10 11:37:04 +0800

    change hclust.method's parameter ward to ward.D1 or ward.D2 in examples

--- vignettes/example-corrRect.hclust.R
+++ vignettes/example-corrRect.hclust.R
@@ -4,7 +4,7 @@ corrplot(M, method = "circle", order = "FPC")
 corrRect(c(5,6))
 
 (order.hc <- corrMatOrder(M, order = "hclust"))
-(order.hc2 <- corrMatOrder(M, order = "hclust", hclust.method = "ward"))
+(order.hc2 <- corrMatOrder(M, order = "hclust", hclust.method = "ward.D2"))
 M.hc  <- M[order.hc, order.hc]
 M.hc2 <- M[order.hc2, order.hc2]
 
@@ -18,14 +18,14 @@ corrRect.hclust(corr = M.hc, k = 2)
 corrplot(M.hc)
 corrRect.hclust(corr = M.hc, k = 3)
 
-# same as: corrplot(M, order = "hclust", hclust.method = "ward", addrect = 2)
+# same as: corrplot(M, order = "hclust", hclust.method = "ward.D2", addrect = 2)
 corrplot(M.hc2)
-corrRect.hclust(M.hc2, k = 2, method = "ward")
+corrRect.hclust(M.hc2, k = 2, method = "ward.D2")
 
-# same as: corrplot(M, order = "hclust", hclust.method = "ward", addrect = 3)
+# same as: corrplot(M, order = "hclust", hclust.method = "ward.D2", addrect = 3)
 corrplot(M.hc2)
-corrRect.hclust(M.hc2, k = 3, method = "ward")
+corrRect.hclust(M.hc2, k = 3, method = "ward.D2")
 
-# same as: corrplot(M, order = "hclust", hclust.method = "ward", addrect = 4)
+# same as: corrplot(M, order = "hclust", hclust.method = "ward.D2", addrect = 4)
 corrplot(M.hc2)
-corrRect.hclust(M.hc2, k = 4, method = "ward")
+corrRect.hclust(M.hc2, k = 4, method = "ward.D2")
