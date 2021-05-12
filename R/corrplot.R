4d205cb6952216214ba880637bb93005ad7711fe
taiyun <weitaiyun@qq.com>
2021-05-12 11:07:19 +0800

    add p.mat&corr names same check

--- R/corrplot.R
+++ R/corrplot.R
@@ -844,6 +844,15 @@ corrplot <- function(corr,
     }
   }
 
+  if(!is.null(p.mat)) {
+    if(!all(colnames(p.mat)==colnames(corr)) |
+       !all(rownames(p.mat)==rownames(corr))) {
+      warning('The p.mat and corr may be not paired,
+              since the rownames and colnames are not totally same.
+              Please check!')
+    }
+  }
+
   if (!is.null(p.mat) && insig != "n") {
     if (order != "original") {
       p.mat <- p.mat[ord, ord]
